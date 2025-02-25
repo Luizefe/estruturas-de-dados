071a898237727b5d648ef28b7caba4549f57282c
Luizefe <81421235+Luizefe@users.noreply.github.com>
2025-02-25 23:04:15 +0000

    meu trabalho quase completo

--- exercise/doubly_linked_list.cpp
+++ exercise/doubly_linked_list.cpp
@@ -1,6 +1,53 @@
 #include "../include/doubly_linked_list.hpp"
+#include <iostream>
 
 int main(int argc, char const* argv[]) {
-  /* code */
+  DoublyLinkedList<int> list;
+
+  list.push_front(10);
+  list.push_back(20);
+  list.push_front(5);
+  list.push_back(30);
+  list.print();
+  
+  std::cout << std::endl <<  list.size() << std::endl;
+  std::cout << "Lista contém 20? " << (list.contains(20) ? "Sim" : "Não") << std::endl;
+
+  for (size_t i = 0; i < list.size(); i++) {
+      std::cout << "Elemento na posição " << i << ": " << list[i] << std::endl;
+  }
+
+  std::cout << std::endl << "Removendo primeiro e último elementos:" << std::endl;
+  list.pop_front();
+  list.pop_back();
+  list.print();
+  
+  //Inseri
+  auto it = list.begin();
+  ++it;
+  list.insert(it, 15);
+  list.print();
+
+  //Remove
+  list.erase(list.begin(), list.end());
+  list.print();
+  
+  std::cout << std::endl << "Lista está vazia? " << (list.empty() ? "Sim" : "Não") << std::endl;
+  
+  //Inseri
+  list.push_back(40);
+  list.push_back(50);
+  list.push_back(60);
+  
+  for (auto it = list.begin(); it != list.end(); ++it) {
+      std::cout << (*it) << " ";
+  }
+  std::cout << std::endl;
+  
+  //Limpa
+  list.clear();
+  list.print();
+  std::cout << "Lista está vazia? " << (list.empty() ? "Sim" : "Não") << std::endl;
+  
   return 0;
 }
--- exercise/linked_list.cpp
+++ exercise/linked_list.cpp
@@ -1,6 +1,50 @@
 #include "../include/linked_list.hpp"
+#include <iostream>
 
 int main(int argc, char const* argv[]) {
-  /* code */
-  return 0;
+  LinkedList<int> list;
+    
+    
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+    std::cout << "Lista após push_front: ";
+    list.print();
+
+    
+    list.insert(1, 15);
+    std::cout << "Lista após insert(1, 15): ";
+    list.print();
+
+    
+    std::cout << "Elemento na posição 2: " << list[2] << std::endl;
+
+    
+    try {
+        int &found = list.find(15);
+        std::cout << "Elemento encontrado: " << found << std::endl;
+    } catch (const std::out_of_range &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    //existência
+    std::cout << "A lista contém 10? " << (list.contains(10) ? "Sim" : "Não") << std::endl;
+    std::cout << "A lista contém 50? " << (list.contains(50) ? "Sim" : "Não") << std::endl;
+
+    //remoção
+    list.pop_front();
+    std::cout << "Lista após pop_front: ";
+    list.print();
+
+    list.remove(1);
+    std::cout << "Lista após remove(1): ";
+    list.print();
+
+    // limpeza
+    list.clear();
+    std::cout << "Lista após clear: ";
+    list.print();
+    std::cout << "A lista está vazia? " << (list.empty() ? "Sim" : "Não") << std::endl;
+
+    return 0;
 }
